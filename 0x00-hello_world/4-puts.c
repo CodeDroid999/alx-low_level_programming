6801e197ef4c3d6cef3a3421a475f14bfe357c8e
CodeDroid999 <johnmwega2002@gmail.com>
2022-10-14 10:30:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  *
- *    * Return: 0
- *
- *     */
-
-
-
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
