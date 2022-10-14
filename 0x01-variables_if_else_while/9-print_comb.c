e7e51758e3bcdb2e11b2a3023bfa7d360a863680
CodeDroid999 <johnmwega2002@gmail.com>
2022-10-14 13:39:19 -0700

    another commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9' ; i++)
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
