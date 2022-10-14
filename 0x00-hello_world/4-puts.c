f1f43a349a5ec8cfd243874d7102539a64bb5048
CodeDroid999 <johnmwega2002@gmail.com>
2022-10-14 09:59:44 -0700

    another commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
