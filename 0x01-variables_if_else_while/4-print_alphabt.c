db74a67a00c20494fdbb24bc4cf3517ee113cba1
CodeDroid999 <johnmwega2002@gmail.com>
2022-10-18 13:24:52 -0700

    all

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Print the alphabet in lowercase letters, except for e and q
- * always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-	
+
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
