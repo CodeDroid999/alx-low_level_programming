80425195c3b065248e1e1b015dfde5eabe8c82de
CodeDroid999 <johnmwega2002@gmail.com>
2022-10-18 09:56:00 -0700

    A C program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+  * main - prints _putchar, fllowd by a new linees
+  * Return: Alwys 0 (Success)
+  */
+
+int main(void)
+
+{
+char abe[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(abe[c]);
+}
+_putchar('\n');
+return (0);
+}
+
