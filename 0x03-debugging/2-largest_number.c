73e8cc2112e0ee8cc593fd3389eef824c6a51fc2
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2022-10-20 12:44:10 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,30 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@CodeDroid999 
-soles21
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x03-debugging/2-largest_number.c
-@soles21
-soles21 all
-Latest commit dec2721 yesterday
- History
- 1 contributor
-Executable File  33 lines (29 sloc)  397 Bytes
-
 #include "main.h"
 
 /**
@@ -58,18 +31,3 @@ int largest_number(int a, int b, int c)
 
 return (largest);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-alx-low_level_programming/2-largest_number.c at master · soles21/alx-low_level_programming
