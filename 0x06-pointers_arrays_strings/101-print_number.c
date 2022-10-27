a6b851882b0bc38615ace50666a71a081dc5d062
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2022-10-28 00:38:49 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		num = -num;
+	} else
+	{
+		n1 = n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	_putchar((num % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
